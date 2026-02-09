e2f196a46be101492c72240c5f77cf824b8eab10
Linuskramshoej <linus.kramshoej@protonmail.com>
2026-02-09 14:50:41 +0100

    TheDiceMan blabla

--- src/theDiceMan.java
+++ src/theDiceMan.java
@@ -1,2 +1,29 @@
+import java.util.Random;
+
 public class theDiceMan {
+    private String activity; // "Heads" eller "Tails"
+    private Random random;
+
+    public theDiceMan() {
+        this.activity = "Eat breakfast";
+        this.random = new Random();
+    }
+
+    public void getActivity() {
+        int result = random.nextInt(6);
+        if (result == 0) {
+            activity = "Heads";
+        } else if (result == 1) {
+            activity = "Tails";
+        } else if (result == 2) {
+            activity = "Tails";
+        } else if (result == 3) {
+            activity = "Tails";
+        } else if (result == 4) {
+            activity = "Tails";
+        } else {
+            activity = "Tails";
+        }
+    }
+
 }
