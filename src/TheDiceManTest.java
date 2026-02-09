42a6949b4598daef65243647ed14c56c53c7d3b9
Jeppeoberg <jeob1000@stud.ek.dk>
2026-02-09 15:21:33 +0100

    jsdkdjb

--- src/TheDiceManTest.java
+++ src/TheDiceManTest.java
@@ -1,9 +1,10 @@
 public class TheDiceManTest {
     static void main() {
         TheDiceMan theDiceMan = new TheDiceMan();
+
 for (int i = 1; i <= 5; i++) {
     theDiceMan.getActivity();
-    System.out.println(theDiceMan.getActivity()
+    System.out.println(theDiceMan);
 }
 
     }
